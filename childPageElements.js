5146b1b8de087e7c7ea841b7e0b1c2971b2890c9
dgdgdgdg <ren.y@dgdgdgdg.com>
2024-09-18 17:51:25 +0900

    Add child page element with custom styling

--- childPageElements.js
+++ childPageElements.js
@@ -1,1 +1,11 @@
-document.querySelector("body").insertAdjacentHTML("beforeend", "<div id='childPageElement'>I am a child page element</div>");
+console.log("childPageElements.js");
+
+const body = document.querySelector("body");
+const message = document.createElement("div");
+message.textContent = "hello";
+message.style.color = "red";
+message.style.textAlign = "center";
+message.style.marginTop = "50px";
+body.appendChild(message);
+
+console.log("childPageElements.js end");
