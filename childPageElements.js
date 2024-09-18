867ce97a3d1a98d8f044e7bf6708a3c2a7ab3d5e
dgdgdgdg <ren.y@dgdgdgdg.com>
2024-09-18 18:25:26 +0900

    Refactor childPageElements.js and improve code readability and maintainability

--- childPageElements.js
+++ childPageElements.js
@@ -1,5 +1,34 @@
 const body = document.querySelector("body");
 
+const linkWrapper = document.createElement("div");
+linkWrapper.style = `
+  position: fixed;
+  bottom: 16px;
+  right: 16px;
+
+  display: flex;
+  flex-direction: column;
+  gap: 8px;
+`;
+body.appendChild(linkWrapper);
+
+const hostName = location.hostname;
+const parentPageLink = document.createElement("a");
+parentPageLink.href = `https://${hostName}`;
+parentPageLink.textContent = "アプリ一覧へ";
+parentPageLink.style = `
+  padding: 8px;
+  background-color: #ffffff;
+  color: #24292e;
+  box-sizing: border-box;
+  border: 2px solid #24292e;
+  border-radius: 4px;
+  text-decoration: none;
+  font-size: 12px;
+  font-family: sans-serif;
+`;
+linkWrapper.appendChild(parentPageLink);
+
 const repoName = location.pathname.split("/")[1];
 const repoLink = document.createElement("a");
 repoLink.href = `https://github.com/dg4-design/${repoName}`;
@@ -7,9 +36,6 @@ repoLink.textContent = "GitHub repository";
 repoLink.target = "_blank";
 repoLink.rel = "noopener noreferrer";
 repoLink.style = `
-  position: fixed;
-  bottom: 16px;
-  right: 16px;
   padding: 8px;
   background-color: #24292e;
   color: #fff;
@@ -18,5 +44,4 @@ repoLink.style = `
   font-size: 12px;
   font-family: sans-serif;
 `;
-
-body.appendChild(repoLink);
+linkWrapper.appendChild(repoLink);
