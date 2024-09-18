8c901df4821bd6b212e9c0ff629341886c79b11f
dgdgdgdg <ren.y@dgdgdgdg.com>
2024-09-18 18:09:07 +0900

    Update child page element styling and positioning

--- childPageElements.js
+++ childPageElements.js
@@ -6,13 +6,13 @@ message.href = `https://github.com/dg4-design/${repoName}`;
 message.textContent = "GitHub repository";
 message.style = `
   position: fixed;
-  bottom: 32px;
-  right: 32px;
-  padding: 10px;
+  bottom: 16px;
+  right: 16px;
+  padding: 8px;
   background-color: #24292e;
   color: #fff;
   text-decoration: none;
-  font-size: 16px;
+  font-size: 12px;
   font-family: sans-serif;
 `;
 
